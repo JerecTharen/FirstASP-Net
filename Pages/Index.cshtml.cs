6a571387fea25ac6b5d5dd2af0c5b90b9d2f04f6
JerecTharen <kf7ags@gmail.com>
2019-04-04 23:04:59 -0600

    initial

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace firstApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public static string helloThere = "Hello There";
+        public void OnGet()
+        {
+            
+        }
+    }
+}
